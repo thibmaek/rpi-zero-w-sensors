421e7d893f4e906664ece1e5fa4bfbb2d5146ebf
Thibault Maekelbergh <thibault.maekelbergh@iCloud.com>
2018-07-07 10:52:49 +0200

    Round to fixed number with 0 decimals

--- routes/sensors/dht.js
+++ routes/sensors/dht.js
@@ -3,8 +3,8 @@ const dht = require('node-dht-sensor');
 module.exports = () => {
   const { temperature, humidity  } = dht.read();
   const response = {
-    temperature: temperature.toFixed(2),
-    humidity: humidity.toFixed(2)
+    temperature: temperature.toFixed(0),
+    humidity: humidity.toFixed(0)
   }
 
   console.log({response});
